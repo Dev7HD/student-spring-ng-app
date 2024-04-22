5d9d5093d72d05011516059aaacb3b4ab6beb7ce
Hamza DAMIRI <hamza.damiri@gmail.com>
2024-04-22 23:46:37 +0100

    Front-end first commit

---
+++ Front-end-angular/src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {ProfileComponent} from "./profile/profile.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import {LoginComponent} from "./login/login.component";
+import {LoadPaymentComponent} from "./load-payment/load-payment.component";
+import {LoadStudentComponent} from "./load-student/load-student.component";
+import {PaymentsComponent} from "./payments/payments.component";
+import {StudentsComponent} from "./students/students.component";
+
+const routes: Routes = [
+  {path: "", component: LoginComponent},
+  {path: "login", component: LoginComponent},
+  {path: "home", component: HomeComponent},
+  {path: "profile", component: ProfileComponent},
+  {path: "dashboard", component: DashboardComponent},
+  {path: "loadPayments", component: LoadPaymentComponent},
+  {path: "loadStudents", component: LoadStudentComponent},
+  {path: "payments", component: PaymentsComponent},
+  {path: "students", component: StudentsComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
